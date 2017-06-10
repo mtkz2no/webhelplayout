d1d24247640564cdf6325862f9d777d10b25f404
mtkz2no <mtkz2no@gmail.com>
2017-06-10 22:42:14 +0900

    support offcanvas sidebar

--- toctreenav.js
+++ toctreenav.js
@@ -10,4 +10,17 @@ $(document).ready(function(){
  $('nav i').click(function(){
   $(this).toggleClass('collapsed');
  });
+ $('#showleft').click(function(){
+  var left = $('#left');
+  if(left.css('display')=='none') {
+   left.removeClass('show');
+   left.toggleClass('show');
+   $('#hideleft')
+    .toggleClass('show');
+  }
+ });
+ $('#hideleft').click(function(){
+  $('#left, #hideleft')
+   .removeClass('show');
+ });
 });
